7befc73eebd0d91fac0f1988c00a8bbcd46ff6b9
myDataScienceTech <69015618+myDataScienceTech@users.noreply.github.com>
2022-12-16 20:21:03 -0500

    Update routing.py

--- backend/controller/routing.py
+++ backend/controller/routing.py
@@ -24,50 +24,5 @@ def get_dijkstra_route(graph, source, destination, max_distance, route_type):
     return path, distance, elevation
 
 def elevation_based_dijkstra(graph, start, end, max_distance, max_bool):
-
-        elevations = collections.defaultdict(int) #elevation dictionary
-        distances = collections.defaultdict(int)  #distance dictionary
-        G = graph
-        Graph_sucessor = G._succ
-        
-        prev = {}
-        max_distance = max_distance
-        nodes_to_visit = [] 
-        visited_elevation = {} 
-        visited_elevation[start] = 0 
-        first_node=(0, 0, start)
-        heappush(nodes_to_visit, first_node)
- 
-        while len(nodes_to_visit)>0:
-            temp_node=heappop(nodes_to_visit)
-            elevation, distance, curr_node = temp_node
-            if curr_node in elevations:
-                continue
-            if curr_node == end:
-                break
-
-            elevations[curr_node] = elevation
-            distances[curr_node] = distance
-            neighbors = Graph_sucessor[curr_node].items()
-            for next_node, next_elevation in neighbors:
-                next_dist = next_elevation[0]['length']
-                next_elevation = abs(G.nodes()[curr_node]['elevation'] - G.nodes()[next_node] ['elevation'])
-                new_distance , new_elevation = distances[curr_node] + next_dist , elevations[curr_node] + next_elevation
-                if new_distance > max_distance:
-                    continue
-
-                if next_node not in visited_elevation or new_elevation<visited_elevation[next_node]:
-                    visited_elevation[next_node] = new_elevation    
-                    if max_bool:
-                        new_elevation *= -1
-                    heappush(nodes_to_visit, [new_elevation, new_distance, next_node])
-                    prev[next_node] = curr_node
-        final_path = []
-        current_node = end
-        final_path.append(current_node)
-        while current_node != start:
-            current_node = prev[current_node]
-            final_path.append(current_node)
-        final_final_path=final_path[::-1]
-        return list(final_final_path)
+    pass
     
